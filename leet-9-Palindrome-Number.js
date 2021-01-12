52077b002e0f625dd4fe22be02153dc39d01bb71
Rustam Ermatov <ermatov_r@auca.kg>
2021-01-12 12:43:23 +0600

    leetCode solutions with descriptions

--- leet-9-Palindrome-Number.js
+++ leet-9-Palindrome-Number.js
@@ -4,37 +4,38 @@ var isPalindrome = function(x) {
 
     for(let i = 0, j = strLength-1; i< Math.floor(strLength / 2); i++, j--){
         if(str.charAt(i) != str.charAt(j)){
-            return false;
+            return;
         }
     }
-    return true;
-
+    return true
 };
+
+isPalindrome(121);
+
 /**
  * @param {number} x
  * @return {boolean}
- *
- * Example 1:
 
+
+ * Example 1:
  Input: x = 121
  Output: true
- Example 2:
 
+ * Example 2:
  Input: x = -121
  Output: false
  Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
- Example 3:
 
+ * Example 3:
  Input: x = 10
  Output: false
  Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
- Example 4:
 
+ * Example 4:
  Input: x = -101
  Output: false
 
-
- Constraints:
-
+ *Constraints:
  -231 <= x <= 231 - 1
+
  */

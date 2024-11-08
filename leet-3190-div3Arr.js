638e476d372ec150faa242e1a5c4a3281c6f36c2
Rustam Ermatov <ermatovrus22@gmail.com>
2024-11-08 10:41:05 -0800

    solved divisible steps to 3

---
+++ leet-3190-div3Arr.js
@@ -0,0 +1,9 @@
+var minimumOperations = function(nums) {
+    let counter = 0;
+    for(let i = 0; i< nums.length; i++){
+        if(nums[i] % 3 === 0){
+            counter++;
+        }
+    }
+    return nums.length - counter
+};

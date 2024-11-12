7b3cfa805ec691af38b55dbe0bfbe1994520f458
Rustam Ermatov <ermatovrus22@gmail.com>
2024-11-12 08:40:16 -0800

    solved valid paranthesis

--- leet-20-valid-parenth.js
+++ leet-20-valid-parenth.js
@@ -1,27 +1,18 @@
 var isValid = function(s) {
-    let sArr = s.split('');
-   let open = ['(', '{' , '['];
-   let close = [')', '}' , ']'];
-   let counter = 0;
-    for(let i = 0; i<s.length - 1; i++){
-        let char = sArr[i];
-        let nextChar = sArr[i+1];
-        let endChar = sArr[s.length - i - 1];
-        if((char === open[0] && nextChar === close[0]) ||
-            (char === open[1] && nextChar === close[1]) ||
-            (char === open[2] && nextChar === close[2]) ){
-            counter++;
-        }else if(i !== s.length / 2){
-            if((char === open[0] && endChar === close[0]) ||
-                (char === open[1] && endChar === close[1]) ||
-                (char === open[2] && endChar === close[2]) ) {
-                counter++;
-                sArr.splice(s.length - i - 1, 1, 'a').join('');
-            }
+    let counter = 0;
+    let length = s.length;
+    while(counter <= length){
+        if(s.includes("()")){
+            s = s.replaceAll("()", "")
+        }
+        if(s.includes("{}")){
+            s = s.replaceAll("{}", "")
+        }
+        if(s.includes("[]")){
+            s = s.replaceAll("[]", "")
         }
 
+        counter++;
     }
-    return(counter === s.length / 2)
+    return s.length === 0;
 };
-
-console.log(isValid("[[[]"));

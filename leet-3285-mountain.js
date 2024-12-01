98af588fc7fa71a0e93ac5aa1c78dbdb4b924862
Rustam Ermatov <ermatovrus22@gmail.com>
2024-11-30 17:54:14 -0800

    solved mountain problem

---
+++ leet-3285-mountain.js
@@ -0,0 +1,9 @@
+var stableMountains = function(height, threshold) {
+    let arr = [];
+    for(let i = 1; i < height.length; i++){
+        if(height[i - 1] > threshold){
+            arr.push(i);
+        }
+    }
+    return arr
+};

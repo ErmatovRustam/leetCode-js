e334c9512cde2ef496142d73c2b253a20fe32d7f
Rustam Ermatov <ermatovrus22@gmail.com>
2024-12-20 17:51:10 -0800

    solved zigzag conversation problem with loop

---
+++ leet-6-zigZag.js
@@ -0,0 +1,26 @@
+var convert = function(s, numRows) {
+    if(numRows === 1) return s
+    s = s.split("");
+    let outArr = [];
+    let ind = 0;
+    let isGrowing = true;
+    for(let i = 0; i < s.length; i++){
+        if(isGrowing){
+            outArr[ind] = (outArr[ind] || "" ) + s[i]
+            ind++;
+            if(ind === numRows){
+                isGrowing = false
+                ind -= 2;
+            }
+        }else{
+             outArr[ind] = (outArr[ind] || "" ) + s[i]
+             ind--;
+             if(ind === - 1){
+                isGrowing = true;
+                ind += 2;
+             }
+        }
+    }
+
+    return outArr.reduce((acc, el) => acc += el)
+};

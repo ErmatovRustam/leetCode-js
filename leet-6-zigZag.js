9db20b3c6c4fe4f1efec413b1e4368221356099f
Rustam Ermatov <ermatovrus22@gmail.com>
2025-01-09 14:28:34 -0800

    updated zigzag probelem

--- leet-6-zigZag.js
+++ leet-6-zigZag.js
@@ -24,30 +24,3 @@ var convert = function(s, numRows) {
 
     return outArr.reduce((acc, el) => acc += el)
 };
-
-// var convert = function(s, numRows) {
-//     if(numRows === 1) return s
-//     s = s.split("");
-//     let outArr = [];
-//     let ind = 0;
-//     let isGrowing = true;
-//     for(let i = 0; i < s.length; i++){
-//         if(isGrowing){
-//             outArr[ind] = (outArr[ind] || "" ) + s[i]
-//             ind++;
-//             if(ind === numRows){
-//                 isGrowing = false
-//                 ind -= 2;
-//             }
-//         }else{
-//              outArr[ind] = (outArr[ind] || "" ) + s[i]
-//              ind--;
-//              if(ind === - 1){
-//                 isGrowing = true;
-//                 ind += 2;
-//              }
-//         }
-//     }
-
-//     return outArr.reduce((acc, el) => acc += el)
-// };
